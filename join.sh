21d56a74736c7f35de266ab896ae7f54b06622d4
Manoel Campos <manoelcampos@gmail.com>
2019-09-01 15:27:53 -0300

    Updates join script

--- join.sh
+++ join.sh
@@ -2,21 +2,21 @@
 
 clear
 OUTDIR="out"
-
 rm -rf $OUTDIR
-i=0
-for DIR in `ls -d */`; do
-   i=$(( i + 1 ))   
-   if [[ i -eq 1 ]]; then
-        echo "$i - Copying first dir '$DIR' to '$OUTDIR' to start joining traces..."
-        cp -r "$DIR" "$OUTDIR" > /dev/null
-   else 
-        echo "$i - Appending traces from '$DIR' to files inside '$OUTDIR' dir..."
-        for FILE in `ls $DIR`; do
-            cat "$DIR$FILE" >> "$OUTDIR/$FILE"
-        done
-   fi
+mkdir $OUTDIR
+
+files=0
+for DIR in `ls -d */ | grep -v $OUTDIR`; do
+     i=$(( i + 1 ))   
+     echo "$i - Appending traces from '$DIR' to files inside '$OUTDIR' dir..."
+     for FILE in `ls $DIR`; do
+          cat "$DIR$FILE" >> "$OUTDIR/$FILE"
+          files=$(( files + 1 ))   
+     done
 done
+
+echo ""
+echo "Total existing files: $files"
 
 
 
